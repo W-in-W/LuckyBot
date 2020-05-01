bd964159d531cd959abce568a3ba3a6e6a8fa296
W-in-W <W-in-W@users.noreply.github.com>
2020-05-01 19:58:44 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LuckyBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
